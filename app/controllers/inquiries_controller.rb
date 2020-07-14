584b137ed9ee7d5a0d480c3ce15cecb9d63b89d5
西岡克弥 <nishikatsu@katsuya.local>
2020-07-14 20:58:09 +0900

    バリテーション前

---
+++ app/controllers/inquiries_controller.rb
@@ -0,0 +1,11 @@
+class InquiriesController < ApplicationController
+  def new
+    @inquiry = Inquiry.new
+  end
+
+  def confirm
+  end
+
+  def thanks
+  end
+end
---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module FashionGood
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+    # 言語の地域を日本に設定
+    config.i18n.default_locale = :ja
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
