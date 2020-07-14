c6554eded727565703eaafbb94506d6b1cc092cd
西岡克弥 <nishikatsu@katsuya.local>
2020-07-15 00:52:49 +0900

    バリテーション後

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,6 +1,6 @@
 # バリテーションを設定していく
 class Inquiry < ApplicationRecord
-  validates :name, presence: true, lenght: { in: 1..160 }
+  validates :name, presence: true, length: { in: 1..160 }
   validates :email, presence: true
   validates :content, length: { maximum: 255 }
 end
